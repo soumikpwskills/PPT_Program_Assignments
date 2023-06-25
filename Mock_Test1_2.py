d91997a673d1328b0349745960a05017cfe15764
soumikpwskills <129747932+soumikpwskills@users.noreply.github.com>
2023-06-25 00:33:40 -0700

    Add files via upload

--- Mock_Test1_2.py
+++ Mock_Test1_2.py
@@ -1,12 +1,10 @@
 def firstUniqueChar(string):
-    fnc = ""
     index = 0
     if len(string) == 0:
         print("EMTPY STRING")
 
     for i in string:
         if string.count(i) == 1:
-            fnc += i
             break
         else:
             index += 1
